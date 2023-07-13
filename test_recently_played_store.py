b95b49cb55e7cc06ec2c8631cc222aef68724cd5
shubham.shelar <shubhamshelar623@gmail.com>
2023-07-13 21:19:09 +0530

    test: added test cases for recently played songs

---
+++ test_recently_played_store.py
@@ -0,0 +1,55 @@
+import pytest
+from recently_played_store import RecentlyPlayedStore
+
+def test_add_song():
+    store = RecentlyPlayedStore(3)
+    store.add_song("user1", "S1")
+    store.add_song("user1", "S2")
+    store.add_song("user1", "S3")
+    store.add_song("user1", "S4")
+    assert store.get_recently_played("user1") == ["S2", "S3", "S4"]
+
+def test_get_recently_played():
+    store = RecentlyPlayedStore(3)
+    store.add_song("user1", "S1")
+    store.add_song("user1", "S2")
+    store.add_song("user1", "S3")
+    assert store.get_recently_played("user1") == ["S1", "S2", "S3"]
+    assert store.get_recently_played("user2") == []
+
+def test_add_song_full_capacity():
+    store = RecentlyPlayedStore(3)
+    store.add_song("user1", "S1")
+    store.add_song("user1", "S2")
+    store.add_song("user1", "S3")
+    store.add_song("user1", "S4")
+    store.add_song("user1", "S5")
+    assert store.get_recently_played("user1") == ["S3", "S4", "S5"]
+
+def test_add_song_multiple_users():
+    store = RecentlyPlayedStore(3)
+    store.add_song("user1", "S1")
+    store.add_song("user1", "S2")
+    store.add_song("user2", "S3")
+    store.add_song("user2", "S4")
+    assert store.get_recently_played("user1") == ["S1", "S2"]
+    assert store.get_recently_played("user2") == ["S3", "S4"]
+
+def test_add_song_empty_store():
+    store = RecentlyPlayedStore(3)
+    store.add_song("user1", "S1")
+    assert store.get_recently_played("user1") == ["S1"]
+
+def test_add_song_nonexistent_user():
+    store = RecentlyPlayedStore(3)
+    store.add_song("user1", "S1")
+    assert store.get_recently_played("user2") == []
+
+def test_print_store(capsys):
+    store = RecentlyPlayedStore(3)
+    store.add_song("user1", "S1")
+    store.add_song("user1", "S2")
+    store.add_song("user2", "S3")
+    store.print_store()
+    captured = capsys.readouterr()
+    assert captured.out == "User: user1\n   - S1\n   - S2\nUser: user2\n   - S3\n"
